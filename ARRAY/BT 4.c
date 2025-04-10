49804ccb394e7c0bf80200b2bcbe60e8c2f5b415
octo0102 <131735672+octo0102@users.noreply.github.com>
2025-04-10 09:25:39 +0700

    Update BT 4.c

--- ARRAY/BT 4.c
+++ ARRAY/BT 4.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+int main(){
+    int x,l1 = 1,l2 = 1;
+    printf("Nhap mang so nguyen va cac so: ");
+    scanf("%d", &x);
+    int a[x];
+    for (int i = 0; i < x; i++ ){
+        scanf("%d", &a[i]);
+    }
+    int min = a[0], max = a[0];
+    for (int i = 1; i < x; i++ ){
+        if ( max < a[i]){ // if first number in array is smaller than second number, set max to second
+            max = a[i];
+            l1++;
+        }
+        if (min > a[i]){ // if first number in array is larger than second number, set min to second
+            min = a[i];
+            l2++;
+        }
+    }
+    printf("Phan tu lon nhat la %d o vi tri %d\nPhan tu be nhat la %d o vi tri %d\n", max, l1, min, l2);
+    return 0;
+}    
